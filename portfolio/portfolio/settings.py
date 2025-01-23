bf092479ab632b45968f1f37edcc510945af17ed
Eric Langlois <elangloiswd@gmail.com>
2025-01-23 10:21:04 -0500

    SAFFSA

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'), 
+#]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'assets/images/'
